9e5c0e157a9fbc8d5a4154a687c5a54403333bf3
DASADEUS <76177815+DASADEUS@users.noreply.github.com>
2024-06-20 19:03:15 +0300

    Upgrade

--- src/predict_app.py
+++ src/predict_app.py
@@ -1,36 +1,39 @@
-"""House price prediction service"""
-
-from flask import Flask, request
+import os
+from flask import Flask, render_template, request
 from flask_cors import CORS
+from joblib import load
+import pandas as pd
 
-app = Flask(__name__)
+MODEL_SAVE_PATH = 'models/my_model_regression_v01.joblib'
+
+# Determine the absolute path to the directory containing templates
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'web'))
+
+app = Flask(__name__, template_folder=template_dir)
 CORS(app)
 
+model = load(MODEL_SAVE_PATH)
 
-def predict(in_data: dict) -> int:
+def predict(total_meters: float) -> int:
     """ Predict house price from input data parameters.
-    :param in_data: house parameters.
+    :param total_meters: house area in square meters.
     :raise Error: If something goes wrong.
     :return: House price, RUB.
     :rtype: int
     """
-    area = float(in_data['area'])
-    AVG_PRICE = 200_000                 # RUB / m2
-    return int(area * AVG_PRICE)
-
+    price = model.predict(pd.DataFrame({'total_meters': [total_meters]}))
+    return int(price.squeeze())
 
 @app.route("/")
 def home():
-    return '<h1>Housing price service.</h1> Use /predict endpoint'
-
+    return render_template('index.html')
 
 @app.route("/predict", methods=['POST'])
 def predict_web_serve():
-    """Dummy service"""
-    in_data = request.get_json()
-    price = predict(in_data)
-    return {'price': price}
-
+    """Endpoint to predict house price."""
+    total_meters = float(request.form['total_meters'])
+    price = predict(total_meters)
+    return render_template('index.html', prediction=f'Predicted price: {price:,} RUB')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
